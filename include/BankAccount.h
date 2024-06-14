16b4b24d42a6778fe97a5a2fd8cfccc1eb9095db
Leead Jacobowitz <leead123@gmail.com>
2024-06-14 13:45:31 +0300

    finished bankaccount class

---
+++ include/BankAccount.h
@@ -0,0 +1,39 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
+#include <string>
+#include <mutex>
+#include <vector>
+
+
+struct TransactionDetails {
+    enum class Type {
+        DEPOSIT,
+        WITHDRAW,
+    };
+    Type typeOfTransaction;
+    std::string transactionDate;
+    double transactionAmount;
+    double balanaceBeforeTransaction;
+    double balanceAfterTransaction;
+};
+
+class BankAccount {
+    private:
+        std::string m_owner;
+        double m_balance;
+        std::vector<TransactionDetails> m_transactionHistory;
+        mutable std::mutex m_mtx;
+
+    public:
+        BankAccount(const std::string& owner, double balance);
+
+        void deposit(double amount);
+        bool withdraw(double amount);
+        double getBalance() const;
+        std::string getOwner() const;
+        std::string getDateString() const;
+        std::vector<TransactionDetails> getTransactionHistory() const;
+
+};
+
+#endif BANKACCOUNT_H
